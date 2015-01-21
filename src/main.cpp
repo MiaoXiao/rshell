72edf96e4b97fc758ff9b9e110bc1249d7df285c
Rica Feng <topgb@msn.com>
2015-01-21 02:13:55 -0800

    Fixed indents, made objectives more clear
    Added many comments to make functions and code clearer
    Revised functions. Untested.

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,26 @@
 
 using namespace std;
 
+//set to true to ignore rest of command
+bool IGNORE = false;
 
-void runCommand(char command)
+//run this command given these parameters
+//returns whether this operation succeeded or not
+bool runCommand(char* executable, char[] argumentList, int connector, bool lastOperation)
 {
-    /*
-    pid = fork();
-    if (execvp(command, argv)!= 0)
-        perror("error in exevp");
-*/
+	/*
+	pid = fork();
+	if (execvp(command, argv)!= 0)
+	perror("error in exevp");
+	*/
 }
 
-//Fixes snip and removes any # and characters after the #
-//Also removes the any ;, ||, or && at the end of the snip
-void fixSnip(char* &snip)
+//Removes any # from a snip, and also any character after said #. Ignore rest of command.
+//Also removes any ; || or && appended at the end of snip
+//returns 0 if ;, 1 if ||, 2 if &&
+//returns -1 if comment
+//returns 3 if no fix needed
+int fixSnip(char* &snip)
 {
 	//do something if these special characters are found
 	char key[] = "#";
@@ -27,83 +34,77 @@ void fixSnip(char* &snip)
 	schar = strpbrk (snip, key);
 	if (schar != NULL)
 	{
-		return true;
+		IGNORE = true;
+		return -1;
 	}
-	return false;
-    if (snip[strlen(snip) - 1] == ';' ||
-        (snip[strlen(snip) - 1] == '|' &&
-        snip[strlen(snip) - 2] == '|') ||
-        (snip[strlen(snip) - 1] == '&' &&
-        snip[strlen(snip) - 2] == '&'))
-    {
-    }
-    else
-    {
-    }
-}
-
-void checkConnector(char* snip)
-{
-
-}
-
-void checkArgumentList()
-{
-
+	else if (snip[strlen(snip) - 1] == ';')
+		return 0;
+	else if (snip[strlen(snip) - 1] == '|' && snip[strlen(snip) - 2] == '|')
+		return 1;
+	else if (snip[strlen(snip) - 1] == '&' && snip[strlen(snip) - 2] == '&')
+		return 2;
+	
+	return 3;
 }
 
 int main(int argc, char* argv[])
 {
 	//user input command
-    char command[256];
+	char command[256];
 	//snippet of entire command
-    char* snip;
-    //executable
-    char* executable;
-    //argument list
-    char* argumentList[256];
+	char* snip;
+	//executable
+	char* executable;
+	//argument list
+	char* argumentList[256];
+	//whether the last operation failed or not
+	bool lastOperation = true;
 
-    do
-    {
-        int count = 0;
-        int argumentListCount = 0;
-
-        //Retrieve command
-        cout << "$ ";
-        cin.getline(command, 256);
-
-        //Split command
-        snip = strtok(command, " ");
-        //Iterate until entire command is parsed and partition command
-        while(snip != NULL)
-        {
-			cout << snip << endl;
-
-            //check snip:
-			if (strcmp(snip, "exit") == 0)
-                return 0;
-			else if (fixSnip(snip))
-            {
-				cout << "comment" << endl;
-            }
-
-            //partition commands
-            while(count <= 1)
-            {
-                switch(count)
-                {
-                    case 0:
-                        executable = snip;
-                        count++;
-                        break;
-                     case 1:
-
-                }
-            }
-            snip = strtok(NULL, " ");
-        }
-
-    } while (1);
+	do
+	{
+		//keep track of whether you are checking executable, argument, or connector
+		// 0 is executable, 1 is argument
+	        int count = 0;
+	        //keep track of size in argument list. resets every command.
+	        int argumentListCount = 0;
+	
+	        //Retrieve command
+	        cout << "$ ";
+	        cin.getline(command, 256);
+	
+	        //Tokenize command
+	        snip = strtok(command, " ");
+	        
+	        //Iterate until entire command is parsed and partitioned
+		while(snip != NULL)
+	        {
+	        	//record keeps track of whether there was a comment, connector, or there was no fix in the last snippet
+			int record;
+			record = fixSnip(snip);
+			
+			//exit shell if input is "exit"
+		        if (strcmp(snip, "exit") == 0)
+				return 0;
+			
+			//if snippet is connector, RUN COMMAND with parameters:
+			//executable, argument list array, connector, whether the last argument failed or not
+			//running the command will return whether the operation failed or not
+			
+			//otherwise, init variable with the executable or argument.
+			//1. if executable, simply init executable
+			//2. if argument, add to argument array and increment argument list counter
+			
+			//if there was a connector
+			
+			//move to next snippet
+			snip = strtok(NULL, " ");
+	        }
+	//RUN command if last snippet was NOT a connector
+	
+	argumentListCount= 0;
+	count = 0;
+	
+	} while (1);
 
     return 0;
 }
