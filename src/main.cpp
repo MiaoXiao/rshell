7edef680a709147855c46f85eee9fa3527f7d151
Rica Feng <rfeng001@ucr.edu>
2015-01-21 21:56:05 -0800

    Added function fixCommand, which changes user input into a a clear statement with spaces seperating each segment

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ bool IGNORE = false;
 
 //run this command given these parameters
 //returns whether this operation succeeded or not
-bool runCommand(char* executable, char[] argumentList, int connector, bool lastOperation)
+bool runCommand(char* executable, char argumentList[], int connector, bool lastOperation)
 {
 	/*
 	pid = fork();
@@ -20,31 +20,52 @@ bool runCommand(char* executable, char[] argumentList, int connector, bool lastO
 	*/
 }
 
-//Removes any # from a snip, and also any character after said #. Ignore rest of command.
-//Also removes any ; || or && appended at the end of snip
-//returns 0 if ;, 1 if ||, 2 if &&
-//returns -1 if comment
-//returns 3 if no fix needed
-int fixSnip(char* &snip)
+//look through command and seperate all statements by spaces
+//if there is a # ignore every character after it
+//returns the fixed command
+void fixCommand(char* command)
 {
-	//do something if these special characters are found
-	char key[] = "#";
-	char* schar;
+    char* fixedCommand;
+    //j keeps track of position in fixed Command
+    //i keeps track of position in command
+    for (int i = 0, j = 0; command[i] != '\0'; ++i, ++j)
+    {
+        if (command[i] == '#')
+        {
+            command[i] = '\0';
+            fixedCommand[j] = '\0';
+        }
+        else if (command[i] == ';')
+        {
+            fixedCommand[j] = ' ';
+            fixedCommand[++j] = ';';
+            fixedCommand[++j] = ' ';
+        }
+        else if (command[i] == '|' && command[i + 1] == '|')
+        {
+            fixedCommand[j] = ' ';
+            fixedCommand[++j] = '|';
+            fixedCommand[++j] = '|';
+            fixedCommand[++j] = ' ';
+            ++i;
+        }
+        else if (command[i] == '&' && command[i + 1] == '&')
+        {
+            fixedCommand[j] = ' ';
+            fixedCommand[++j] = '&';
+            fixedCommand[++j] = '&';
+            fixedCommand[++j] = ' ';
+            ++i;
+        }
+        else
+        {
+            fixedCommand[j] = command[i];
+        }
 
-	schar = strpbrk (snip, key);
-	if (schar != NULL)
-	{
-		IGNORE = true;
-		return -1;
-	}
-	else if (snip[strlen(snip) - 1] == ';')
-		return 0;
-	else if (snip[strlen(snip) - 1] == '|' && snip[strlen(snip) - 2] == '|')
-		return 1;
-	else if (snip[strlen(snip) - 1] == '&' && snip[strlen(snip) - 2] == '&')
-		return 2;
-	
-	return 3;
+        if (command[i + 1] == '\0')
+            fixedCommand[j + 1] = '\0';
+    }
+    strcpy(command, fixedCommand);
 }
 
 int main(int argc, char* argv[])
@@ -54,56 +75,52 @@ int main(int argc, char* argv[])
 	//snippet of entire command
 	char* snip;
 	//executable
-	char* executable;
+	char* executable = NULL;
 	//argument list
-	char* argumentList[256];
+	char* argumentList;
 	//whether the last operation failed or not
 	bool lastOperation = true;
 
 	do
 	{
-		//keep track of whether you are checking executable, argument, or connector
-		// 0 is executable, 1 is argument
+	    	//keep track of whether you are checking executable, argument, or connector
+	    	// 0 is executable, 1 is argument
 	        int count = 0;
 	        //keep track of size in argument list. resets every command.
 	        int argumentListCount = 0;
-	
+
 	        //Retrieve command
 	        cout << "$ ";
 	        cin.getline(command, 256);
-	
+
+            //fix command, add any neccessary spaces to seperate statements
+            fixCommand(command);
+
 	        //Tokenize command
 	        snip = strtok(command, " ");
-	        
+
 	        //Iterate until entire command is parsed and partitioned
-		while(snip != NULL)
+            while(snip != NULL)
 	        {
-	        	//record keeps track of whether there was a comment, connector, or there was no fix in the last snippet
-			int record;
-			record = fixSnip(snip);
-			
-			//exit shell if input is "exit"
+			    //exit shell if input is "exit"
 		        if (strcmp(snip, "exit") == 0)
-				return 0;
-			
-			//if snippet is connector, RUN COMMAND with parameters:
-			//executable, argument list array, connector, whether the last argument failed or not
-			//running the command will return whether the operation failed or not
-			
-			//otherwise, init variable with the executable or argument.
-			//1. if executable, simply init executable
-			//2. if argument, add to argument array and increment argument list counter
-			
-			//if there was a connector
-			
-			//move to next snippet
-			snip = strtok(NULL, " ");
+			    	return 0;
+
+			    //if snippet is connector, RUN COMMAND with parameters:
+
+			    //otherwise, init variable with the executable or argument.
+			    //1. if executable, simply init executable
+			    //2. if argument, add to argument array and increment argument list counter
+
+
+			    //move to next snippet
+			    snip = strtok(NULL, " ");
 	        }
 	//RUN command if last snippet was NOT a connector
-	
+
 	argumentListCount= 0;
 	count = 0;
-	
+
 	} while (1);
 
     return 0;
