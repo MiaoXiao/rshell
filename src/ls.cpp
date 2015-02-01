ebc3f6e1d459feb0be699a401f98ca0391ba0327
Rica Feng <rfeng001@ucr.edu>
2015-01-31 20:46:08 -0800

    Fixed bugs in parsing command line arguments in ls.cpp. Can now distinguih between path or flags, and correctly error checks

--- src/ls.cpp
+++ src/ls.cpp
@@ -10,20 +10,33 @@
 
 using namespace std;
 
+//checks for possible path
+//returns false if the first character was a -
+bool checkPath(char* command)
+{
+	char* pch;
+	pch = strtok(command, " ");
+	return (pch[0] != '-');
+}
+
 //parses commands to find flags, fills flags vector
-//returns true/false if invalid command
+//returns false if invalid command
 bool parseCommand(char* command, vector<bool> &flags)
 {
 	//snippet
 	char* pch;
 
 	pch = strtok(command, " ");
+
 	while (pch != NULL)
 	{
 		//make sure flag starts with -
-		if (pch[1] == '-')
+		if (pch[0] == '-')
 		{
-			for (int i = 1; pch[i] != ' '; i++)
+			if (pch[1] == '\0')
+				return false;
+
+			for (int i = 1; pch[i] != '\0'; i++)
 			{
 				if(pch[i] == 'a')
 					flags[0] = true;
@@ -37,7 +50,7 @@ bool parseCommand(char* command, vector<bool> &flags)
 			pch = strtok(NULL, " ");
 		}
 		else
-			return false;	
+			return false;
 	}
 	return true;
 }
@@ -58,8 +71,35 @@ int main(int argc, char *argv[])
 	vector<bool> flags(false);
 	flags.resize(3);
 
-	//directory name
+	//default directory name
 	char dirName[] = ".";
+	
+	if (argc < 1) //error
+	{
+		cout << "Error opening file" << endl;
+	}
+	else if (argc > 1) //run ls with path or flags
+	{
+		int i = 1;
+		//initalize potential path
+		if (checkPath(argv[i]))
+		{
+			//cout << "path assigned" << endl;
+			strcpy(argv[i], dirName);	
+			i++;
+		}
+
+		for (; i < argc; i++)
+		{
+			if (!parseCommand(argv[i], flags))
+			{
+				cout << "Invalid flag arguments" << endl;
+				exit(1);
+			}
+		}
+		//displayFlags(flags);
+	}
+	//else, run normal ls
 
 	//error check opendir
 	if (opendir(dirName) == NULL)
@@ -78,7 +118,7 @@ int main(int argc, char *argv[])
 		cout << direntp->d_name << endl; // use stat here to find attributes of file
 		
 		//error check closedir
-		if (closedir(dirp) != 0)
-			perror("Error with closedir()");
+		//if (closedir(dirp) != 0)
+		//	perror("Error with closedir()");
 	}
 }
