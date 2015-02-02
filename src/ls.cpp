be494889e4a695ec8443d62da66c670fdfa80a0f
Rica Feng <rfeng001@ucr.edu>
2015-02-01 20:21:48 -0800

    Added basic -l functionality. Does not display username and group name, but uid and gid. Bytes shown, last day and time modified (without dls) also shown.

--- src/ls.cpp
+++ src/ls.cpp
@@ -6,23 +6,84 @@
 #include <cstdlib>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <time.h>
 #include <string.h>
 #include <vector>
 #include <iostream>
 
-
 using namespace std;
+
+//given month number, returns month name
+string getMonth(unsigned monthnumber)
+{
+	switch(monthnumber)
+	{
+		case 0:
+			return "Jan";
+		case 1:
+			return "Feb";
+		case 2:
+			return "March";
+		case 3:
+			return "April";
+		case 4:
+			return "May";
+		case 5:
+			return "June";
+		case 6:
+			return "July";
+		case 7:
+			return "Aug";
+		case 8:
+			return "Oct";
+		case 9:
+			return "Sep";
+		case 10:
+			return "Nov";
+		case 11:
+			return "Dec";
+	}
+	return "Error getting month name";
+}
 
 //displays files
 //checks flags vector for anything extra
 void displayls(dirent* filename, const vector<bool> flags)
 {
-	//if -a flag is not set, do not ls that file
+	//if -a flag is not set, do not ls a file that starts with . 
 	if (filename->d_name[0] != '.' || flags[0])
 	{
 		//check for potential -l flag
 		if (flags[1])
 		{
+			//get -l info on
+			struct stat info;
+
+			//perror
+			if (stat(filename->d_name, &info) == -1)
+			{
+				perror("There was a problem with stat()");
+				exit(1);
+			}
+
+			//get time info
+			struct tm lastmodified;
+			lastmodified = *gmtime(&info.st_mtime);
+			//check for day light savings
+			bool dls = false;
+			if (lastmodified.tm_isdst > 0)
+				dls = true;
+			
+			//user id
+			cout << info.st_uid << "\t";
+			//group id
+			cout << info.st_gid << "\t";
+			//size in bytes
+			cout << info.st_size << "\t";
+			//date of last modified
+			cout << getMonth(lastmodified.tm_mon) << " " << lastmodified.tm_mday <<  "\t";
+			//time of last modified
+			cout << lastmodified.tm_hour - dls << ":" << lastmodified.tm_min << "\t";
 
 		}
 		//display name
@@ -30,6 +91,7 @@ void displayls(dirent* filename, const vector<bool> flags)
 
 	}
 }
+
 
 //checks for possible path
 //returns false if the first character was a -
@@ -130,6 +192,7 @@ int main(int argc, char *argv[])
 	//open directory and assign pointer
 	DIR *dirp = opendir(dirName);	
 	dirent *direntp;
+
 
 	while ((direntp = readdir(dirp)))
 	{
