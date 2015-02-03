231d3bb135b11eb29c7c37957beb8b6dc6016f5b
Rica Feng <topgb@msn.com>
2015-02-02 22:27:20 -0800

    Major update
    Can now accept multiple paths along with optional flags, in no particular order. Will only output an error
    If more than 1 path, will list the directory name before listing the files
    Will output a newline between listing different paths
    Shortened some control statements to 1 line
    Directory now correctly closes and error checks
    Fixed memory leak involving the use of char*

--- src/ls.cpp
+++ src/ls.cpp
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <cstring>
 #include <vector>
 #include <iostream>
 
@@ -18,32 +19,20 @@ string getMonth(unsigned monthnumber)
 {
 	switch(monthnumber)
 	{
-		case 0:
-			return "Jan";
-		case 1:
-			return "Feb";
-		case 2:
-			return "March";
-		case 3:
-			return "April";
-		case 4:
-			return "May";
-		case 5:
-			return "June";
-		case 6:
-			return "July";
-		case 7:
-			return "Aug";
-		case 8:
-			return "Oct";
-		case 9:
-			return "Sep";
-		case 10:
-			return "Nov";
-		case 11:
-			return "Dec";
+		case 0: return "Jan";
+		case 1: return "Feb";
+		case 2: return "March";
+		case 3: return "April";
+		case 4: return "May";
+		case 5: return "June";
+		case 6: return "July";
+		case 7: return "Aug";
+		case 8: return "Oct";
+		case 9:	return "Sep";
+		case 10: return "Nov";
+		case 11: return "Dec";
 	}
-	return "Error getting month name";
+	return "ERROR";
 }
 
 //displays files
@@ -69,10 +58,6 @@ void displayls(dirent* filename, const vector<bool> flags)
 			//get time info
 			struct tm lastmodified;
 			lastmodified = *gmtime(&info.st_mtime);
-			//check for day light savings
-			bool dls = false;
-			if (lastmodified.tm_isdst > 0)
-				dls = true;
 			
 			//user id
 			cout << info.st_uid << "\t";
@@ -83,57 +68,36 @@ void displayls(dirent* filename, const vector<bool> flags)
 			//date of last modified
 			cout << getMonth(lastmodified.tm_mon) << " " << lastmodified.tm_mday <<  "\t";
 			//time of last modified
-			cout << lastmodified.tm_hour - dls << ":" << lastmodified.tm_min << "\t";
-
+			cout << lastmodified.tm_hour << ":" << lastmodified.tm_min << "\t";
 		}
 		//display name
 		cout << filename->d_name << endl; 	
-
 	}
 }
 
-
-//checks for possible path
-//returns false if the first character was a -
-bool checkPath(char* command)
-{
-	char* pch;
-	pch = strtok(command, " ");
-	return (pch[0] != '-');
-}
-
 //parses commands to find flags, fills flags vector
-//returns false if invalid command
-bool parseCommand(char* command, vector<bool> &flags)
+//fills a container with paths
+//returns whether flags are invalid
+bool parseCommand(char* command, vector<bool> &flags, vector<string> &paths)
 {
-	//snippet
-	char* pch;
-
-	pch = strtok(command, " ");
-
-	while (pch != NULL)
+	//if command is a flag
+	if (command[0] == '-')
 	{
-		//make sure flag starts with -
-		if (pch[0] == '-')
+		if (command[1] == '\0') return false;
+		
+		for (int i = 1; command[i] != '\0'; i++)
 		{
-			if (pch[1] == '\0')
-				return false;
-
-			for (int i = 1; pch[i] != '\0'; i++)
-			{
-				if(pch[i] == 'a')
-					flags[0] = true;
-				else if (pch[i] == 'l')
-					flags[1] = true;
-				else if (pch[i] == 'R')
-					flags[2] = true;
-				else
-					return false;
-			}
-			pch = strtok(NULL, " ");
+			if(command[i] == 'a') flags[0] = true;
+			else if (command[i] == 'l') flags[1] = true;
+			else if (command[i] == 'R') flags[2] = true;
+			else return false;
 		}
-		else
-			return false;
+	}
+	else //if command is path
+	{
+		string s(command);
+		paths.push_back(s);
+		return true;
 	}
 	return true;
 }
@@ -146,6 +110,13 @@ void displayFlags(const vector<bool> flags)
 	cout << "-R: " << flags[2] << endl;
 }
 
+//DEBUG: displays paths array
+void displayPaths(const vector<string> v)
+{
+	for (int i = 0; i < v.size(); i++) cout << i << ": " << v[i] << endl;
+	if (!v.size()) cout << "paths vector empty" << endl;
+}
+
 int main(int argc, char *argv[])
 {
 	//pos 0 is -a flag
@@ -153,58 +124,71 @@ int main(int argc, char *argv[])
 	//pos 2 is -R flag
 	vector<bool> flags(false);
 	flags.resize(3);
-
-	//default directory name
-	char dirName[] = ".";
 	
+	//container to hold paths
+	vector<string> paths;
+
 	if (argc < 1) //error
 	{
 		cout << "Error opening file" << endl;
+		exit(1);
 	}
 	else if (argc > 1) //run ls with path or flags
 	{
-		int i = 1;
-		//initalize potential path
-		if (checkPath(argv[i]))
+		for (int i = 1; i < argc; i++)
 		{
-			strcpy(dirName, argv[i]);	
-			i++;
-		}
-
-		for (; i < argc; i++)
-		{
-			if (!parseCommand(argv[i], flags))
+			//check command to see if path, flag, or error.
+			//fills corresponding container
+			//also updates size of paths array
+			if (!parseCommand(argv[i], flags, paths))
 			{
-				cout << "Invalid flag arguments" << endl;
+				cout << "Invalid flag" << endl;
 				exit(1);
 			}
 		}
 		//displayFlags(flags);
 	}
-	//if argc is 1, run normal ls
-
-	//error check opendir
-	if (opendir(dirName) == NULL)
-	{
-		perror("Error with opendir()");
-		exit(1);
-	}
-	//open directory and assign pointer
-	DIR *dirp = opendir(dirName);	
-	dirent *direntp;
-
-
-	while ((direntp = readdir(dirp)))
-	{
-		//error check readdir
-		if (direntp < 0)
-			perror("Error with readdir()");
+    
+	//if no paths were given, only use the default path
+	if (!paths.size()) paths.push_back(".");
 	
-		//list files
-		displayls(direntp, flags);
-
-		//error check closedir
-		//if (closedir(dirp) != 0)
-		//	perror("Error with closedir()");
+	//displayPaths(paths);
+    
+	//iterate through any directory in the path
+	for (int i = 0; i < paths.size(); i++)
+	{
+		//convert string to c_str
+		char dirName[paths[i].size() + 1];
+		strncpy(dirName, paths[i].c_str(), paths[i].size());
+		
+		//error check opendir
+		if (opendir(dirName) == NULL)
+		{
+			perror("Error with opendir()");
+			exit(1);
+		}
+		//open directory and assign pointer
+		DIR *dirp = opendir(dirName);	
+		dirent *direntp;
+		
+		//show directory name if multiple paths
+		if (paths.size() > 1) cout << paths[i] << ": " << endl;
+		    
+		while ((direntp = readdir(dirp)))
+		{
+			//error check readdir
+			if (direntp < 0) perror("Error with readdir()");
+			//list files
+			displayls(direntp, flags);
+		}
+		
+		if (closedir(dirp) == -1)
+		{
+			perror("Error with closedir()");
+			exit(1);
+		}
+		
+		//new line for next directory (except for last directory)
+		if (i + 1 != paths.size()) cout << endl;
 	}
 }
