9f27c963866e526780337d9baf9684ac39080576
Rica Feng <rfeng001@ucr.edu>
2015-02-07 13:30:32 -0800

    inode number now displays correctly. added appropriate error checking, added extra permission checking for file mode, stat will now properly display files in another directory

--- src/ls.cpp
+++ src/ls.cpp
@@ -37,13 +37,20 @@ string getMonth(unsigned monthnumber)
 		case 10: return "Nov";
 		case 11: return "Dec";
 	}
-	return "ERROR";
+	return "Error with getting month name";
 }
 
 //returns string of file permissions
 string filePermission(mode_t m)
 {
-	string permissions = "---------- 1";
+	string permissions = "----------";
+	if (S_ISDIR(m)) permissions[0] = 'd';
+	else if (S_ISBLK(m)) permissions[0] = 'b';
+	else if (S_ISCHR(m)) permissions[0] = 'c';
+	else if (S_ISLNK(m)) permissions[0] = 'l';
+	else if (S_ISFIFO(m)) permissions[0] = 'p';
+	else if (S_ISSOCK(m)) permissions[0] = 's';
+
 	if (m & S_IRUSR) permissions[1] = 'r';
 	if (m & S_IWUSR) permissions[2] = 'w';
 	if (m & S_IXUSR) permissions[3] = 'x';
@@ -54,39 +61,143 @@ string filePermission(mode_t m)
 	if (m & S_IWOTH) permissions[8] = 'w';
 	if (m & S_IXOTH) permissions[9] = 'x';
 	
-	if (S_ISDIR(m))
+	return permissions;
+}
+
+/*
+//fill paths vector with new paths based on -R flag
+void findallpaths(vector<string> &paths, string &pathsofar)
+{
+	//check if you must check only the . dir, or every dir
+	vector<string> recursepaths;
+	for (unsigned i = 0; i < paths.size(); ++i)
 	{
-		permissions[0] = 'd';
-		permissions[11] = '2'; 
+		if (paths[i] == ".")
+		{
+			recursepaths.push_back(paths[i]);
+			i = paths.size();
+		}
 	}
 
-	return permissions;
+	//if no . directory found, recurse on all paths given
+	//if (!recursepaths.size()) recursepaths = paths;
+
+	//begin reursing through all possible directories
+	for (unsigned i = 0; i < paths.size(); ++i)
+	{
+		pathsofar = paths[i];
+
+		DIR *dirp = opendir(paths[i].c_str());
+		dirent *direntp;
+		struct stat info;
+		while((direntp = readdir(dirp)))
+		{
+			stat(direntp->d_name, &info);
+			if (S_ISDIR(info.st_mode))
+			{
+				pathsofar += "/";
+				pathsofar.append(direntp->d_name);
+				findallpaths(paths, pathsofar);
+			}
+		}
+		closedir(dirp);	
+	}
+	
+}
+*/
+
+//returns number of hard links
+int findhardlinks(string dir, bool flaga)
+{
+	struct stat inodeinfo;
+	int numbhardlinks = 2;
+
+	//error check opendir
+	DIR *tdirp = opendir(dir.c_str());
+	if (tdirp == NULL)
+	{
+		perror("Error with opendir()");
+		exit(1);
+	}
+
+	dirent *tdirentp;
+	//cout << "checking dir: " << dir << endl;
+	while ((tdirentp = readdir(tdirp)))
+	{
+		if (tdirentp < 0)
+		{
+			perror("Error with readdir()");
+			exit(1);
+		}
+
+		//cout << "file in dir: " << tdirentp->d_name << endl;
+
+		//check a flag
+		if (tdirentp->d_name[0] != '.' || flaga)
+		{
+			//insert path at start of string
+			string fileindir(tdirentp->d_name);
+			//make sure path is not . or ..
+			if (fileindir != "." && fileindir != "..")
+			{
+				fileindir.insert(0, dir + "/");
+				//error check stat
+				if (stat(fileindir.c_str(), &inodeinfo) == -1)
+				{
+					perror("Error with stat()");
+					exit(1);
+				}
+				//if current file is a directory, increase number of hard links
+				if (S_ISDIR(inodeinfo.st_mode))
+				{
+					++numbhardlinks;
+				//	cout << endl <<  "file added: " << tdirentp->d_name << endl;
+				}
+			}
+		}
+	}
+	//error check closed dir
+	if (closedir(tdirp) == -1)
+	{
+		perror("Error with closedir()");
+		exit(1);
+	}
+
+	return numbhardlinks;
 }
 
 //displays all files in a vector files
 //checks flags vector for anything extra
-void displayls(vector<string> filenames, const vector<bool> flags)
+void displayls(vector<string> filenames, const vector<bool> flags, string currentpath)
 {
+	//struct for storing stat info
+	struct stat info;
 	for (unsigned i = 0; i < filenames.size(); ++i)
 	{
+		//cout << "Filename: " << filenames[i] << endl;	
 		//if -a flag is not set, do not ls a file that starts with . 
 		if (filenames[i].at(0) != '.' || flags[0])
 		{
 			//check for potential -l flag
 			if (flags[1])
 			{
-				//get -l info on
-				struct stat info;
+				//insert current path at start of string
+				filenames[i].insert(0, currentpath + "/");
+				//cout << "checking file: " << filenames[i] << endl;
 
 				//error check stat
-				if (stat(filenames[i].c_str(), &info) == -1)
+				if (stat(filenames[i].c_str(), &info) != 0)
 				{
-					perror("There was a problem with stat()");
+					perror("Error with stat()");
 					exit(1);
 				}
 
-				//file permissions and inode
+				//file permissionse
 				cout << filePermission(info.st_mode) << "\t";
+
+				//inode number
+				(!S_ISDIR(info.st_mode)) ? cout << 1 : cout << findhardlinks(filenames[i], flags[0]);
+				cout << "\t";
 
 				//user id
 				struct passwd userinfo = *getpwuid(info.st_uid);
@@ -158,10 +269,8 @@ void displayVector(const vector<string> v)
 //comparator for sort
 bool scompare(string a, string b)
 {
-	for (unsigned i = 0; i < a.size(); ++i)
-		a[i] = tolower(a[i]);
-	for (unsigned i = 0; i < b.size(); ++i)
-		b[i] = tolower(b[i]);
+	for (unsigned i = 0; i < a.size(); ++i) a[i] = tolower(a[i]);
+	for (unsigned i = 0; i < b.size(); ++i) b[i] = tolower(b[i]);
 	return a < b;
 }
 
@@ -207,6 +316,10 @@ int main(int argc, char *argv[])
 	//create pointer to directory to be read
 	dirent *direntp;
 
+//	string pathsofar = "";
+	//if -R flag passed in, find all potential paths
+//	if (flags[2]) findallpaths(paths, pathsofar);
+
 	//iterate through every path 
 	for (unsigned i = 0; i < paths.size(); ++i)
 	{
@@ -215,21 +328,29 @@ int main(int argc, char *argv[])
 
 		//open directory and assign pointer
 		DIR *dirp = opendir(paths[i].c_str());
-		if (dirp == NULL) perror ("Error with opendir()");
+		if (dirp == NULL)
+		{
+			perror ("Error with opendir()");
+			exit(1);
+		}
 	
 		//fill filenames vector with all file names
 		while ((direntp = readdir(dirp)))
 		{
 			//error check readdir
-			if (direntp < 0) perror("Error with readdir()");
+			if (direntp < 0)
+			{
+				perror("Error with readdir()");
+				exit(1);
+			}
 			string s(direntp->d_name);
 			filenames.push_back(s);
 		}
 		
 		//alphabetize filenames
 		sort(filenames.begin(), filenames.end(), scompare);
-		//list files, based on file names
-		displayls(filenames, flags);
+		//list files, based on flags, for the given path 
+		displayls(filenames, flags, paths[i]);
 
 		//close directory
 		if (closedir(dirp) == -1)
