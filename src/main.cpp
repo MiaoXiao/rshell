cc0226ba5510ff8ea5d847390cd18216581ec120
Rica Feng <rfeng001@ucr.edu>
2015-02-23 00:19:07 -0800

    all redirections

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ struct task
 //returns fd of new file
 int openFile(const char* filename, const bool append, const bool create)
 {
-	int fd;
+	int fd = -1;
 	if (create && append) fd = open(filename, O_APPEND | O_WRONLY | O_CREAT, 00600);
 	else if (create && !append) fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
 	else if (!create && append) fd = open(filename, O_APPEND | O_WRONLY);
@@ -99,6 +99,8 @@ void runCommand(vector<task> taskList, Kirb &K)
 	int w;
 	//holds all files to write to
 	vector<int> writeto;
+	//vector of all pipe seperators
+	vector<int> allPipes;
 	//short cut for taskList[i].seperatorPos
 	int sepPos;
 	//will be false if tasks processing should halt
@@ -126,26 +128,45 @@ void runCommand(vector<task> taskList, Kirb &K)
 				for(unsigned j = 0; j < taskList[i].seperatorPos.size(); ++j)
 				{
 					sepPos = taskList[i].seperatorPos[j];
-					cout << "seperator: " << taskList[i].seperators[j] << endl;
-					cout << "opening: " << taskList[i].argumentList[i + sepPos] << endl;
+				//	cout << "seperator: " << taskList[i].seperators[j] << endl;
 					switch(taskList[i].seperators[j])
-					{	
+					{
+						case 7:
+						case 8:
+						case 9:
 						case 3: // >
+							//cout << "opening: " << taskList[i].argumentList[i + sepPos] << endl;
 							w = openFile(taskList[i].argumentList[sepPos], false, true);
 							break;
 						case 4: // <
+							//cout << "opening: " << taskList[i].argumentList[i + sepPos] << endl;
 							w = openFile(taskList[i].argumentList[sepPos], false, false);  
 							if (w == -1) redirection = false;
 							break;
+						case 10:
+						case 11:
+						case 12:
 						case 5: // >>
+							//cout << "opening: " << taskList[i].argumentList[i + sepPos] << endl;
 							w = openFile(taskList[i].argumentList[sepPos], true, true);
 							break;
-						case 6:
+						case 6: // |
 							break;
 					}
 					writeto.push_back(w);
 				}
 			}
+			
+			/*//set up files for piping
+			int rw[2];
+			if (pipe(rw) == -1)
+			{
+				perror("There was an error with pipe()");
+				exit(1);
+			}
+			writeto.push_back(rw[0]);
+			writeto.push_back(rw[1]);
+			*/
 
 			//start forking process
 			int pid = fork();
@@ -156,18 +177,30 @@ void runCommand(vector<task> taskList, Kirb &K)
 			}
 			else if (pid == 0) //child
 			{
+				int spot;
 				//write to correct file if using redirection
 				if (redirection)
 				{
 					for (unsigned j = 0; j < taskList[i].seperatorPos.size(); ++j)
 					{
+						if (taskList[i].seperators[j] == 7 || taskList[i].seperators[j] == 10) spot = 0;
+						else if (taskList[i].seperators[j] == 9 || taskList[i].seperators[j] == 12) spot = 2;
+						else spot = 1;
+
 						sepPos = taskList[i].seperatorPos[j];
+						//cout << "seppos: " << sepPos << endl;
 						switch(taskList[i].seperators[j])
 						{
+							case 7:
+							case 8:
+							case 9:
+							case 10:
+							case 11:
+							case 12:
 							case 3: // > 
 							case 5: // >>
-								closeCheck(1);
-								dupCheck(writeto[j], 1);
+								closeCheck(spot);
+								dupCheck(writeto[j], spot);
 								taskList[i].argumentList[sepPos] = '\0';
 								break;
 							case 4: // <
@@ -175,7 +208,13 @@ void runCommand(vector<task> taskList, Kirb &K)
 								dupCheck(writeto[j], 0);
 								taskList[i].argumentList[sepPos + 1] = '\0';
 								break;
-							case 6:
+							case 6: // |
+							/*
+								closeCheck(1);
+								closeCheck(rw[1]);
+								dupCheck(rw[0], 1);
+								taskList[i].argumentList[sepPos] = '\0';
+								*/
 								break;
 						}
 					}
@@ -184,24 +223,57 @@ void runCommand(vector<task> taskList, Kirb &K)
 				//try to run the executable/arguments
 				if (execvp(taskList[i].argumentList[0], taskList[i].argumentList) == -1)
 					perror("There was an error with the executable or argument list");
-				
+
 				exit(1);
 			}
 			else if (pid > 0) //parent
 			{
 				if (waitpid(pid, &status, 0) == -1)	perror("Error with waitpid");
 				
-				//close fds
-				if (redirection)
+				/*
+				for (unsigned j = 0; j < taskList[i].seperatorPos.size(); ++j)
 				{
-					for(unsigned j = 0; j < writeto.size(); ++j)
+					//if piping, fork a second time
+					if (taskList[i].seperators[j] == 6)
 					{
-						closeCheck(writeto[j]);
+						sepPos = taskList[i].seperatorPos[j];
+						pid = fork();
+						if (pid <= -1) //error
+						{
+							perror("There was an error with fork()");
+							exit(1);
+						}
+						else if (pid == 0) //second child
+						{
+							closeCheck(0);
+							closeCheck(rw[0]);
+							dupCheck(rw[1], 0);
+						
+							//test, fill new arg list
+							char* newarglist[MEMORY];
+							newarglist[0] = taskList[i].argumentList[sepPos];
+							newarglist[1] = '\0';
+
+							//try to run the executable/arguments
+							if (execvp(taskList[i].argumentList[sepPos], newarglist) == -1)
+								perror("There was an error with the executable or argument list");
+							
+							exit(1);
+						}
+						else //second parent
+						{
+							if (waitpid(pid, &status, 0) == -1)	perror("Error with waitpid");
+						}
 					}
+				
 				}
+*/	
+				//close all used fds, including any pipes
+				if (redirection)
+					for(unsigned j = 0; j < writeto.size(); ++j) closeCheck(writeto[j]);
 			}
 			writeto.clear();
-
+			//allPipes.clear();
 		}
 		else
 		{
@@ -277,6 +349,20 @@ void fixCommand(char* command)
 			fixedCommand[++j] = '<';
 			fixedCommand[++j] = ' ';
         }
+        else if ((command[i] == '0' || command[i] == '1' || command[i] == '2') &&
+				command[i + 1] == '>')
+        {
+            if (command[i] == '0') fixedCommand[j] = '0';
+            else if (command[i] == '1') fixedCommand[j] = '1';
+            else if (command[i] == '2') fixedCommand[j] = '2';
+            fixedCommand[++j] = '>';
+            if (command[i + 2] == '>')
+            {
+				++i;
+				fixedCommand[++j] = '>';
+            }
+            ++i;
+        }
         else fixedCommand[j] = command[i];
 
         if (command[i + 1] == '\0') fixedCommand[j + 1] = '\0';
@@ -307,7 +393,14 @@ int checkSeperator(const char* snip)
     if (!strcmp(snip, ">")) return 3;
     else if (!strcmp(snip, "<")) return 4;
     else if (!strcmp(snip, ">>")) return 5;
-    else if (!strcmp(snip, "|")) return 6;
+    else if (!strcmp(snip, "||")) return 6;
+    else if (!strcmp(snip, "0>")) return 7;
+    else if (!strcmp(snip, "1>")) return 8;
+    else if (!strcmp(snip, "2>")) return 9;
+    else if (!strcmp(snip, "0>>")) return 10;
+    else if (!strcmp(snip, "1>>")) return 11;
+    else if (!strcmp(snip, "2>>")) return 12;
+
 	return -1;	
 }
 
@@ -366,9 +459,9 @@ int main(int argc, char* argv[])
 
         //partition command, add any necessary spaces to separate statements
         fixCommand(command);
-
+		
 		//check if nothing is entered
-		if (!strcmp(command, "\0")) finishTask = false; 
+		//if (!strcmp(command, "\0")) finishTask = false; 
 
 	    //Tokenize command
 	    snip = strtok(command, "\t ");
@@ -432,10 +525,11 @@ int main(int argc, char* argv[])
                 //if last task, run all tasks
                 if (snip == NULL)
                 {
-                	displayCharArray(t.argumentList);
-                	cout << "size of seperators: " << t.seperators.size() << endl;
-                	cout << "size of arglist: " << t.argumentListSize << endl;
-                    runCommand(taskList, K);
+                	//displayCharArray(t.argumentList);
+                	//cout << "size of seperators: " << t.seperators.size() << endl;
+                	//cout << "size of arglist: " << t.argumentListSize << endl;
+                    if (!t.argumentListSize == 0)runCommand(taskList, K);
+                    else cout << "No executable specified" << endl;
                     //all tasks finished, get new tasks from command prompt
                     finishTask = false;
                     processTask = false;
